0873cf222128791c6e77469023dbd6a681bc3fd2
Leonardo Vaz <49069816+vaz42@users.noreply.github.com>
2019-03-30 16:20:46 -0300

    Changed class name

--- List1/IntMultp.java
+++ List1/IntMultp.java
@@ -1,4 +1,4 @@
-public class Intmultp{
+public class IntMultp{
     public static void main(String[] args){
         System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[1]));
     }
